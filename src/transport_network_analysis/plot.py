a5185134ee4054b8b01dee9eb82338d583239cce
VANOORD\MRV <mark.vankoningsveld@vanoord.com>
2019-01-12 12:22:02 +0100

    Added kml visualisation to plot module in package

---
+++ src/transport_network_analysis/plot.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import datetime
+
+# plotting libraries
+from plotly.offline import init_notebook_mode, iplot
+import plotly.graph_objs as go
+
+# spatial libraries 
+import pyproj
+import shapely.geometry
+from simplekml import Kml, Style
+
+def vessel_planning(vessels, activities, colors, web=False):
+        """create a plot of the planning of vessels"""
+
+        def get_segments(series, activity, y_val):
+            """extract 'start' and 'stop' of activities from log"""
+            x = []
+            y = []
+            for i, v in series.iteritems():
+                if v == activity + ' start':
+                    start = i
+                if v == activity + ' stop':
+                    x.extend((start, start, i, i, i))
+                    y.extend((y_val, y_val, y_val, y_val, None))
+            return x, y
+
+        # organise logdata into 'dataframes' 
+        dataframes = []
+        for vessel in vessels:
+            df = pd.DataFrame(
+                {'log_value': vessel.value, 'log_string': vessel.log}, vessel.t)
+            dataframes.append(df)
+        df = dataframes[0]
+
+        # prepare traces for each of the activities
+        traces = []
+        for i, activity in enumerate(activities):
+            x_combined = []
+            y_combined = []
+            for k, df in enumerate(dataframes):
+                y_val = vessels[k].name
+                x, y = get_segments(
+                    df['log_string'], activity=activity, y_val=y_val)
+                x_combined.extend(x)
+                y_combined.extend(y)
+            traces.append(go.Scatter(
+                name=activity,
+                x=x_combined,
+                y=y_combined,
+                mode='lines',
+                hoverinfo='y+name',
+                line=dict(color=colors[i], width=10),
+                connectgaps=False))
+        
+        # prepare layout of figure
+        layout = go.Layout(
+            title='Vessel planning',
+            hovermode='closest',
+            legend=dict(x=0, y=-.2, orientation="h"),
+            xaxis=dict(
+                title='Time',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f'),
+                range=[0, vessel.t[-1]]),
+            yaxis=dict(
+                title='Vessels',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f')))
+        
+        # plot figure
+        init_notebook_mode(connected=True)        
+        fig = go.Figure(data=traces, layout=layout)
+        return iplot(fig, filename='news-source')
+
+def vessel_kml(env, vessels, 
+               fname='vessel_movements.kml',
+               icon='http://maps.google.com/mapfiles/kml/shapes/donut.png',
+               size=1,
+               scale=1):
+        """Create a kml visualisation of vessels. Env variable needs to contain 
+        epoch to enable conversion of simulation time to real time. Vessels need
+        logs that contain geometries in lat, lon as a function of time."""
+ 
+        # create a kml file containing the visualisation
+        kml = Kml()
+        fol = kml.newfolder(name="Vessels")
+
+        shared_style = Style()
+        shared_style.labelstyle.color = 'ffffffff'  # White
+        shared_style.labelstyle.scale = size  
+        shared_style.iconstyle.color = 'ffff0000'  # Blue
+        shared_style.iconstyle.scale = scale
+        shared_style.iconstyle.icon.href = icon
+
+        # each timestep will be represented as a single point
+        for vessel in vessels:
+            for log_index, value in enumerate(vessel.log["Geometry"][:-1]):
+                
+                begin = env.epoch + datetime.timedelta(seconds=vessel.log["Timestamp"][log_index])
+                end = env.epoch + datetime.timedelta(seconds=vessel.log["Timestamp"][log_index + 1])
+                
+                pnt = fol.newpoint(name=vessel.name, coords=[(vessel.log["Geometry"][log_index].x, vessel.log["Geometry"][log_index].y)])
+                pnt.timespan.begin = begin.isoformat()
+                pnt.timespan.end = end.isoformat()
+                pnt.style = shared_style
+
+            # include last point as well
+            begin = env.epoch + datetime.timedelta(seconds=vessel.log["Timestamp"][log_index + 1])
+            end = env.epoch + datetime.timedelta(seconds=vessel.log["Timestamp"][log_index + 1])
+           
+            pnt = fol.newpoint(name=vessel.name, coords=[(vessel.log["Geometry"][log_index + 1].x, vessel.log["Geometry"][log_index + 1].y)])
+            pnt.timespan.begin = begin.isoformat()
+            pnt.timespan.end = end.isoformat()
+            pnt.style = shared_style
+                
+        kml.save(fname)
