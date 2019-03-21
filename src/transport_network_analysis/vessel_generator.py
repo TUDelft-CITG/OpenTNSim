25faa2f523ffe2d6f2678202a16bb62b051a2b56
Joris den Uijl <jorisdenuijl@gmail.com>
2019-03-21 15:19:30 +0100

    WIP

--- src/transport_network_analysis/vessel_generator.py
+++ src/transport_network_analysis/vessel_generator.py
@@ -18,6 +18,9 @@ import shapely.geometry
 import datetime, time
 import random
 
+# import core from self
+import transport_network_analysis.core as core
+
 logger = logging.getLogger(__name__)
 
 
@@ -34,14 +37,14 @@ class VesselGenerator:
 
         random.seed(random_seed)
     
-    def generate(self, environment, name, path = None, scenario = None):
+    def generate(self, environment, vessel_name, path = None, scenario = None):
         """ Generate a vessel """
 
         vessel_info = self.vessel_database.sample(n = 1, random_state = int(1000 * random.random()))
         vessel_data = {}
 
         vessel_data["env"] = environment
-        vessel_data["name"] = name
+        vessel_data["name"] = vessel_name
 
         if scenario:
             vessel_info = vessel_info[vessel_info["scenario"] == scenario]
@@ -57,3 +60,69 @@ class VesselGenerator:
             vessel_data["geometry"] = nx.get_node_attributes(environment.FG, "geometry")[path[0]]
         
         return self.vessel_type(**vessel_data)
+    
+    def arrival_process(self, environment, arrival_distribution = 1, arrival_process = "Markovian"):
+        """ 
+        Make arrival process
+        
+        environment:            simpy environment
+        arrival_distribution:   specify the distribution from which vessels are generated, int or list
+        arrival_process:        process of arrivals
+        """
+
+        # Create an array with inter-arrival times -- average number of seconds between arrivals
+        if type(arrival_distribution) == int:
+            self.inter_arrival_times = [3600 / arrival_distribution] * 24
+        
+        elif type(arrival_distribution) == list and len(arrival_distribution) == 24:
+            self.inter_arrival_times = [3600 / n for n in arrival_distribution]
+        
+        elif type(arrival_distribution) == list:
+            raise ValueError("List should contain an average number of vessels per hour for an entire day: 24 entries")
+        
+        else:
+            raise ValueError("Specify an arrival distribution: type Integer or type List")
+        
+        while True:
+
+            # Check simulation time
+            inter_arrival = self.inter_arrival_times[datetime.datetime.fromtimestamp(environment.now).hour]
+
+            # In the case of a Markovian arrival process
+            if arrival_process == "Markovian":
+
+                # Make a timestep based on the poisson process
+                time = random.expovariate(inter_arrival)
+                
+                environment.arrivals.append(time)
+                yield environment.timeout(time)
+
+                # Create a vessel
+                vessel = environment.vessel_generator.generate()
+
+                # Move on path
+                
+
+
+class Simulation(core.SimpyObject, core.Identifiable):
+    """
+    A class to generate vessels from a database
+    """
+
+    def __init__(self, vessel_generator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """ Initialization """
+
+        self.vessel_generator = vessel_generator
+    
+    def run(self, duration = 24 * 60 * 60, scenario = None):
+        """ 
+        Run the simulation 
+        
+        duration:               specify the duration of the simulation in seconds
+        scenario:               scenario with vessels - should be coupled to the database
+        """ 
+
+    
+
+
