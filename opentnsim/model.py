b7f6b6e079be3d5a30990f1951356c08b4cb0c69
Mark van Koningsveld <m.vankoningsveld@tudelft.nl>
2019-07-04 22:38:14 +0200

    moved to lower level

---
+++ opentnsim/model.py
@@ -0,0 +1,192 @@
+"""Vessel generator."""
+
+# package(s) related to time, space and id
+import json
+import logging
+import uuid
+
+# you need these dependencies (you can get these from anaconda)
+# package(s) related to the simulation
+import simpy
+import networkx as nx
+
+# spatial libraries
+import pyproj
+import shapely.geometry
+
+# additional packages
+import datetime, time
+import pandas as pd
+import random
+
+# import core from self
+import transport_network_analysis.core as core
+
+logger = logging.getLogger(__name__)
+
+
+class VesselGenerator:
+    """
+    A class to generate vessels from a database
+    """
+
+    def __init__(self, vessel_type, vessel_database, loaded = None, random_seed = 4):
+        """ Initialization """
+
+        self.vessel_type = vessel_type
+        self.vessel_database = vessel_database
+        self.loaded = loaded
+
+        random.seed(random_seed)
+    
+    
+    def generate(self, environment, vessel_name, scenario = None):
+        """ Generate a vessel """
+
+        vessel_info = self.vessel_database.sample(n = 1, random_state = int(1000 * random.random()))
+        vessel_data = {}
+
+        vessel_data["env"] = environment
+        vessel_data["name"] = vessel_name
+
+        if scenario:
+            vessel_info = vessel_info[vessel_info["scenario"] == scenario]
+
+        for key in vessel_info:
+            if key == "vessel_id":
+                vessel_data["id"] = vessel_info[key].values[0]
+            else:
+                vessel_data[key] = vessel_info[key].values[0]
+        
+        if self.loaded == True:
+            vessel_data["level"] = vessel_data["capacity"]
+        elif self.loaded == "Random":
+            if random.random() < 0.5:
+                vessel_data["level"] = vessel_data["capacity"]
+            else:
+                vessel_data["level"] = 0
+
+        vessel_data["route"] = None
+        vessel_data["geometry"] = None
+        
+        return self.vessel_type(**vessel_data)
+    
+    
+    def arrival_process(self, environment, origin, destination, arrival_distribution, scenario, arrival_process):
+        """ 
+        Make arrival process
+        
+        environment:            simpy environment
+        arrival_distribution:   specify the distribution from which vessels are generated, int or list
+        arrival_process:        process of arrivals
+        """
+
+        # Create an array with inter-arrival times -- average number of seconds between arrivals
+        if type(arrival_distribution) == int:
+            self.inter_arrival_times = [3600 / arrival_distribution] * 24
+        
+        elif type(arrival_distribution) == list and len(arrival_distribution) == 24:
+            self.inter_arrival_times = [3600 / n for n in arrival_distribution]
+        
+        elif type(arrival_distribution) == list:
+            raise ValueError("List should contain an average number of vessels per hour for an entire day: 24 entries.")
+        
+        else:
+            raise ValueError("Specify an arrival distribution: type Integer or type List.")
+
+        while True:
+
+            # Check simulation time
+            inter_arrival = self.inter_arrival_times[datetime.datetime.fromtimestamp(environment.now).hour]
+
+            # In the case of a Markovian arrival process
+            if arrival_process == "Markovian":
+
+                # Make a timestep based on the poisson process
+                yield environment.timeout(random.expovariate(1 / inter_arrival))
+            
+            elif arrival_process == "Uniform":
+
+                # Make a timestep based on uniform arrivals
+                yield environment.timeout(inter_arrival)
+            
+            else:
+                raise ValueError("No other arrival processes are yet defined. You can add them to transport_network_analysis/vessel_generator.py.")
+
+            # Create a vessel
+            vessel = self.generate(environment, "Vessel", scenario)
+            
+            # Check if a route is available with vessel properties
+            width, height, draught = vessel.width, vessel.current_height, vessel.current_draught
+            route = None
+
+            for i in environment.routes.index:
+                if (environment.routes["Origin"][i] == origin and \
+                    environment.routes["Destination"][i] == destination and \
+                    environment.routes["Width"][i] == width and \
+                    environment.routes["Height"][i] == height and \
+                    environment.routes["Depth"][i] == draught):
+                    route = environment.routes["Route"][i]
+            
+            if route: 
+                vessel.route = route
+                vessel.geometry = nx.get_node_attributes(environment.FG, "geometry")[vessel.route[0]]
+            else:
+                route = vessel.get_route(origin = origin, destination = destination)
+                environment.routes = environment.routes.append({"Origin": origin, 
+                                                                "Destination": destination, 
+                                                                "Width": width, 
+                                                                "Height": height, 
+                                                                "Depth": draught, 
+                                                                "Route": route}, ignore_index = True)
+
+                vessel.route = route
+                vessel.geometry = nx.get_node_attributes(environment.FG, "geometry")[vessel.route[0]]
+                
+            environment.vessels.append(vessel)
+            
+            # Move on path
+            environment.process(vessel.move())
+
+
+class Simulation(core.Identifiable):
+    """
+    A class to generate vessels from a database
+    """
+
+    def __init__(self, simulation_start, graph, scenario = None):
+        """ 
+        Initialization 
+        
+        scenario: scenario with vessels - should be coupled to the database
+        """
+        self.environment = simpy.Environment(initial_time = time.mktime(simulation_start.timetuple()))
+        self.environment.FG = graph
+        self.environment.routes = pd.DataFrame.from_dict({"Origin": [], "Destination": [], "Width": [], "Height": [], "Depth": [], "Route": []})
+        self.scenario = scenario
+
+        self.environment.vessels = []
+    
+    def add_vessels(self, vessel_generator, origin, destination, arrival_distribution = 1, arrival_process = "Markovian"):
+        """ 
+        Make arrival process
+        
+        environment:            simpy environment
+        arrival_distribution:   specify the distribution from which vessels are generated, int or list
+        arrival_process:        process of arrivals
+        """
+
+        self.environment.process(vessel_generator.arrival_process(self.environment, origin, destination, arrival_distribution, self.scenario, arrival_process))
+    
+    def run(self, duration = 24 * 60 * 60):
+        """ 
+        Run the simulation 
+        
+        duration:               specify the duration of the simulation in seconds
+        """ 
+        
+        self.environment.run(until = self.environment.now + duration)
+
+    
+
+
---
+++ opentnsim/plot.py
@@ -0,0 +1,332 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+# plotting libraries
+from plotly.offline import init_notebook_mode, iplot
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+
+# spatial libraries 
+import pyproj
+import shapely.geometry
+from simplekml import Kml, Style
+
+import networkx as nx
+
+def vessel_planning(vessels, activities, colors, web=False):
+        """create a plot of the planning of vessels"""
+
+        def get_segments(series, activity, y_val):
+            """extract 'start' and 'stop' of activities from log"""
+            x = []
+            y = []
+            for i, v in series.iteritems():
+                if v == activity + ' start':
+                    start = i
+                if v == activity + ' stop':
+                    x.extend((start, start, i, i, i))
+                    y.extend((y_val, y_val, y_val, y_val, None))
+            return x, y
+
+        # organise logdata into 'dataframes' 
+        dataframes = []
+        for vessel in vessels:
+            df = pd.DataFrame(
+                {'log_value': vessel.log["Value"], 'log_string': vessel.log["Message"]}, vessel.log["Timestamp"])
+            dataframes.append(df)
+        df = dataframes[0]
+
+        # prepare traces for each of the activities
+        traces = []
+        for i, activity in enumerate(activities):
+            x_combined = []
+            y_combined = []
+            for k, df in enumerate(dataframes):
+                y_val = vessels[k].name
+                x, y = get_segments(
+                    df['log_string'], activity=activity, y_val=y_val)
+                x_combined.extend(x)
+                y_combined.extend(y)
+            traces.append(go.Scatter(
+                name=activity,
+                x=x_combined,
+                y=y_combined,
+                mode='lines',
+                hoverinfo='y+name',
+                line=dict(color=colors[i], width=10),
+                connectgaps=False))
+        
+        # prepare layout of figure
+        layout = go.Layout(
+            title='Vessel planning',
+            hovermode='closest',
+            legend=dict(x=0, y=-.2, orientation="h"),
+            xaxis=dict(
+                title='Time',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f'),
+                range=[0, vessel.log["Timestamp"][-1]]),
+            yaxis=dict(
+                title='Vessels',
+                titlefont=dict(
+                    family='Courier New, monospace',
+                    size=18,
+                    color='#7f7f7f')))
+        
+        # plot figure
+        init_notebook_mode(connected=True)        
+        fig = go.Figure(data=traces, layout=layout)
+        return iplot(fig, filename='news-source')
+
+def vessel_kml(env, vessels, 
+               fname='vessel_movements.kml',
+               icon='http://maps.google.com/mapfiles/kml/shapes/sailing.png',
+               size=1,
+               scale=1,
+               stepsize=120):
+        """Create a kml visualisation of vessels. Env variable needs to contain 
+        epoch to enable conversion of simulation time to real time. Vessels need
+        logs that contain geometries in lat, lon as a function of time."""
+ 
+        # create a kml file containing the visualisation
+        kml = Kml()
+        fol = kml.newfolder(name="Vessels")
+
+        shared_style = Style()
+        shared_style.labelstyle.color = 'ffffffff'  # White
+        shared_style.labelstyle.scale = size  
+        shared_style.iconstyle.color = 'ffff0000'  # Blue
+        shared_style.iconstyle.scale = scale
+        shared_style.iconstyle.icon.href = icon
+
+        # each timestep will be represented as a single point
+        for vessel in vessels:
+            geom_x = []
+            geom_y = []
+
+            for geom in vessel.log["Geometry"]:
+                geom_x.append(geom.x)
+                geom_y.append(geom.y)
+
+            vessel.log["Geometry - x"] = geom_x
+            vessel.log["Geometry - y"] = geom_y
+
+            time_stamp_min = min(vessel.log["Timestamp"]).timestamp()
+            time_stamp_max = max(vessel.log["Timestamp"]).timestamp()
+
+            steps = int(np.floor((time_stamp_max - time_stamp_min) / stepsize))
+            timestamps_t = np.linspace(time_stamp_min, time_stamp_max, steps)
+
+            times = []
+            for t in vessel.log["Timestamp"]:
+                times.append(t.timestamp())
+
+            vessel.log["timestamps_t"] = timestamps_t
+            vessel.log["timestamps_x"] = np.interp(timestamps_t, times, vessel.log["Geometry - x"])
+            vessel.log["timestamps_y"] = np.interp(timestamps_t, times, vessel.log["Geometry - y"])
+
+            for log_index, value in enumerate(vessel.log["timestamps_t"][:-1]):
+                
+                begin = datetime.datetime.fromtimestamp(vessel.log["timestamps_t"][log_index])
+                end = datetime.datetime.fromtimestamp(vessel.log["timestamps_t"][log_index + 1])
+                
+                pnt = fol.newpoint(name=vessel.name, coords=[(vessel.log["timestamps_x"][log_index], vessel.log["timestamps_y"][log_index])])
+                pnt.timespan.begin = begin.isoformat()
+                pnt.timespan.end = end.isoformat()
+                pnt.style = shared_style
+
+            # include last point as well
+            begin = datetime.datetime.fromtimestamp(vessel.log["timestamps_t"][log_index + 1])
+            # end = datetime.datetime.fromtimestamp(vessel.log["timestamps_t"][log_index + 1])
+           
+            pnt = fol.newpoint(name=vessel.name, coords=[(vessel.log["timestamps_x"][log_index + 1], vessel.log["timestamps_y"][log_index + 1])])
+            pnt.timespan.begin = begin.isoformat()
+            # pnt.timespan.end = end.isoformat()
+            pnt.style = shared_style
+                
+        kml.save(fname)
+
+def site_kml(env, sites, 
+               fname='site_development.kml',
+               icon='http://maps.google.com/mapfiles/kml/shapes/square.png',
+               size=1,
+               scale=3,
+               stepsize=120):
+        """Create a kml visualisation of vessels. Env variable needs to contain 
+        epoch to enable conversion of simulation time to real time. Vessels need
+        logs that contain geometries in lat, lon as a function of time."""
+
+        # create a kml file containing the visualisation
+        kml = Kml()
+        fol = kml.newfolder(name="Sites")
+
+        # each timestep will be represented as a single point
+        for site in sites:
+            for log_index, value in enumerate(site.log["Timestamp"][:-1]):
+                style = Style()
+                style.labelstyle.color = 'ffffffff'  # White
+                style.labelstyle.scale = 1  
+                style.iconstyle.color = 'ff00ffff'  # Yellow
+                style.iconstyle.scale = scale*(site.log["Value"][log_index]/site.container.capacity)
+                style.iconstyle.icon.href = icon
+
+                begin = site.log["Timestamp"][log_index]
+                end = site.log["Timestamp"][log_index + 1]
+
+                pnt = fol.newpoint(name=site.name, coords=[(site.log["Geometry"][log_index].x,
+                                                            site.log["Geometry"][log_index].y)])
+                pnt.timespan.begin = begin.isoformat()
+                pnt.timespan.end = end.isoformat()
+                pnt.style = style
+
+            # include last point as well
+            style = Style()
+            style.labelstyle.color = 'ffffffff'  # White
+            style.labelstyle.scale = 1  
+            style.iconstyle.color = 'ff00ffff'  # Yellow
+            style.iconstyle.scale = scale*(site.log["Value"][log_index+1]/site.container.capacity)
+            style.iconstyle.icon.href = icon
+
+            begin = site.log["Timestamp"][log_index + 1]
+            # end = site.log["Timestamp"][log_index + 1]
+           
+            pnt = fol.newpoint(name=site.name, coords=[(site.log["Geometry"][log_index + 1].x, 
+                                                        site.log["Geometry"][log_index + 1].y)])
+            pnt.timespan.begin = begin.isoformat()
+            # pnt.timespan.end = end.isoformat()
+            pnt.style = style
+                
+        kml.save(fname)
+
+def graph_kml(env, 
+              fname='graph.kml',
+              icon='http://maps.google.com/mapfiles/kml/shapes/donut.png',
+              size=0.5,
+              scale=0.5,
+              width=5):
+        """Create a kml visualisation of graph. Env variable needs to contain 
+        graph."""
+ 
+        # create a kml file containing the visualisation
+        kml = Kml()
+        fol = kml.newfolder(name="Vessels")
+
+        shared_style = Style()
+        shared_style.labelstyle.color = 'ffffffff'  # White
+        shared_style.labelstyle.scale = size  
+        shared_style.iconstyle.color = 'ffffffff'  # White
+        shared_style.iconstyle.scale = scale
+        shared_style.iconstyle.icon.href = icon
+        shared_style.linestyle.color = 'ff0055ff'  # Red
+        shared_style.linestyle.width = width
+
+        nodes = list(env.FG.nodes)
+        
+        # each timestep will be represented as a single point
+        for log_index, value in enumerate(list(env.FG.nodes)[0:-1-1]):
+
+            pnt = fol.newpoint(name='', 
+                               coords=[(nx.get_node_attributes(env.FG, "geometry")[nodes[log_index]].x,
+                                        nx.get_node_attributes(env.FG, "geometry")[nodes[log_index]].y)])
+            pnt.style = shared_style
+
+        edges = list(env.FG.edges)
+        for log_index, value in enumerate(list(env.FG.edges)[0:-1-1]):
+
+            lne = fol.newlinestring(name='',
+                                    coords = [(nx.get_node_attributes(env.FG, "geometry")[edges[log_index][0]].x,
+                                               nx.get_node_attributes(env.FG, "geometry")[edges[log_index][0]].y),
+                                              (nx.get_node_attributes(env.FG, "geometry")[edges[log_index][1]].x,
+                                               nx.get_node_attributes(env.FG, "geometry")[edges[log_index][1]].y)])
+            lne.style = shared_style
+                
+        kml.save(fname)
+
+def energy_use(vessel, testing = False):
+    energy_use_loading = 0            # concumption between loading start and loading stop
+    energy_use_sailing_full = 0       # concumption between sailing full start and sailing full stop
+    energy_use_unloading = 0          # concumption between unloading  start and unloading  stop
+    energy_use_sailing_empty = 0      # concumption between sailing empty start and sailing empty stop
+    energy_use_waiting = 0            # concumption between waiting start and waiting stop
+
+    for i in range(len(vessel.log["Message"])):
+        if vessel.log["Message"][i] == "Energy use loading":
+            energy_use_loading += vessel.log["Value"][i]
+
+        elif vessel.log["Message"][i] == "Energy use sailing full":
+            energy_use_sailing_full += vessel.log["Value"][i]
+
+        elif vessel.log["Message"][i] == "Energy use unloading":
+            energy_use_unloading += vessel.log["Value"][i]
+
+        elif vessel.log["Message"][i] == "Energy use sailing empty":
+            energy_use_sailing_empty += vessel.log["Value"][i]
+        
+        elif vessel.log["Message"][i] == "Energy use waiting":
+            energy_use_waiting += vessel.log["Value"][i]
+
+    # For the total plot
+    fig, ax1 = plt.subplots(figsize = [15, 10])
+
+    # For the barchart
+    height = [energy_use_loading, 
+            energy_use_unloading, 
+            energy_use_sailing_full, 
+            energy_use_sailing_empty,
+            energy_use_waiting]
+    labels = ["Loading", 
+            "Unloading", 
+            "Sailing full", 
+            "Sailing empty",
+            "Waiting"]
+    colors = [(55/255,126/255,184/255), 
+            (98/255, 192/255, 122/255), 
+            (255/255,150/255,0/255), 
+            (98/255, 141/255, 122/255),
+            (124/255, 10/255, 2/255)]
+
+    positions = np.arange(len(labels))
+    ax1.bar(positions, height, color = colors)
+
+    # For the cumulative percentages
+    total_use = sum([energy_use_loading, 
+                    energy_use_unloading, 
+                    energy_use_sailing_full, 
+                    energy_use_sailing_empty,
+                    energy_use_waiting])
+
+    energy_use_unloading += energy_use_loading
+    energy_use_sailing_full += energy_use_unloading
+    energy_use_sailing_empty += energy_use_sailing_full
+    energy_use_waiting += energy_use_sailing_empty
+    y = [energy_use_loading, 
+        energy_use_unloading, 
+        energy_use_sailing_full, 
+        energy_use_sailing_empty, 
+        energy_use_waiting]
+    n = [energy_use_loading / total_use,
+        energy_use_unloading / total_use,
+        energy_use_sailing_full / total_use,
+        energy_use_sailing_empty / total_use,
+        energy_use_waiting / total_use,]
+
+    ax1.plot(positions, y, 'ko', markersize=10)
+    ax1.plot(positions, y, 'k')
+
+    for i, txt in enumerate(n):
+        x_txt = positions[i] + 0.1
+        y_txt = y[i] * 0.95
+        ax1.annotate("{:02.1f}%".format(txt * 100), 
+                    (x_txt, y_txt), size = 12)
+
+    # Further markup
+    plt.ylabel("Energy useage in KWH", size = 12)
+    ax1.set_xticks(positions)
+    ax1.set_xticklabels(labels, size = 12)
+    plt.title("Energy use - {}".format(vessel.name), size = 15)
+
+    if testing == False:
+        plt.show()
