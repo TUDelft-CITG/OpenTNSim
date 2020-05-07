1e00e43c9c775b082e9adedc283279e8126b573f
Mark van Koningsveld <m.vankoningsveld@tudelft.nl>
2020-05-07 18:34:25 +0200

    Prepare for release of version 1.0.0

--- opentnsim/__init__.py
+++ opentnsim/__init__.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "transport-network-analysis"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "0.0.1"
-finally:
-    del get_distribution, DistributionNotFound
+"""Top-level package for OpenTNSim."""
+
+__author__ = """Mark van Koningsveld"""
+__email__ = "M.vanKoningsveld@tudelft.nl"
+__version__ = "1.0.0"
