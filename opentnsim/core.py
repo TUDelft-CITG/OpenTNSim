c72689c975b7edc8cab9f57a540a150d980f5034
Mark van Koningsveld <mark.vankoningsveld@gmail.com>
2021-01-27 00:11:02 +0100

    Updated branche

--- opentnsim/core.py
+++ opentnsim/core.py
@@ -10,6 +10,7 @@ import uuid
 import simpy
 import random
 import networkx as nx
+import numpy as np
 
 # spatial libraries
 import pyproj
@@ -23,7 +24,6 @@ logger = logging.getLogger(__name__)
 
 class SimpyObject:
     """General object which can be extended by any class requiring a simpy environment
-
     env: a simpy Environment
     """
 
@@ -33,14 +33,12 @@ class SimpyObject:
 
 
 class HasResource(SimpyObject):
-    """HasProcessingLimit class
-
-    Adds a limited Simpy resource which should be requested before the object is used for processing."""
+    """Something that has a resource limitation, a resource request must be granted before the object can be used.
+    nr_resources: nr of requests that can be handled simultaneously"""
 
     def __init__(self, nr_resources=1, priority=False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         """Initialization"""
-
         self.resource = (
             simpy.PriorityResource(self.env, capacity=nr_resources)
             if priority
@@ -49,8 +47,7 @@ class HasResource(SimpyObject):
 
 
 class Identifiable:
-    """Something that has a name and id
-
+    """Mixin class: Something that has a name and id
     name: a name
     id: a unique id generated with uuid"""
 
@@ -63,8 +60,7 @@ class Identifiable:
 
 
 class Locatable:
-    """Something with a geometry (geojson format)
-
+    """Mixin class: Something with a geometry (geojson format)
     geometry: can be a point as well as a polygon"""
 
     def __init__(self, geometry, *args, **kwargs):
@@ -76,7 +72,7 @@ class Locatable:
 
 class Neighbours:
     """Can be added to a locatable object (list)
-    
+
     travel_to: list of locatables to which can be travelled"""
 
     def ___init(self, travel_to, *args, **kwargs):
@@ -86,11 +82,11 @@ class Neighbours:
 
 
 class HasContainer(SimpyObject):
-    """Container class
-
+    """Mixin class: Something with a storage capacity
     capacity: amount the container can hold
     level: amount the container holds initially
-    container: a simpy object that can hold stuff"""
+    container: a simpy object that can hold stuff
+    total_requested: a counter that helps to prevent over requesting"""
 
     def __init__(self, capacity, level=0, total_requested=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -108,8 +104,7 @@ class HasContainer(SimpyObject):
 
 
 class Log(SimpyObject):
-    """Log class
-
+    """Mixin class: Something that has logging capability
     log: log message [format: 'start activity' or 'stop activity']
     t: timestamp
     value: a value can be logged as well
@@ -142,15 +137,20 @@ class Log(SimpyObject):
 
 
 class VesselProperties:
-    """
-    Add information on possible restrictions to the vessels.
-    Height, width, etc.
+    """Mixin class: Something that has vessel properties
+    vessel_type: can contain info on vessel type (avv class, cemt_class or other)
+    width: vessel width
+    length: vessel length
+    height_empty: vessel height unloaded
+    height_full: vessel height loaded
+    draught_empty: draught unloaded
+    draught_full: draught loaded
+    Add information on possible restrictions to the vessels, i.e. height, width, etc.
     """
 
     def __init__(
         self,
         vessel_type,
-        installed_power,
         width,
         length,
         height_empty,
@@ -164,7 +164,6 @@ class VesselProperties:
 
         """Initialization"""
         self.vessel_type = vessel_type
-        self.installed_power = installed_power
 
         self.width = width
         self.length = length
@@ -258,51 +257,25 @@ class VesselProperties:
 
 
 class HasEnergy:
-    """
-    Add information on energy use and effects on energy use.
+    """Mixin class: Something that has energy usage.
+    installed_power: installed engine power [kW]
+    resistance: Rtot unloaded [N]
+    resistance_empty: Rtot loaded [N]
+    emissionfactor: emission factor [-]
     """
 
-    def __init__(self, emissionfactor, *args, **kwargs):
+    def __init__(self, installed_power, resistance, resistance_empty, emissionfactor, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         """Initialization"""
+        self.installed_power = installed_power
+        self.resistance = resistance
+        self.resistance_empty = resistance_empty
         self.emissionfactor = emissionfactor
-        self.energy_use = {"total": 0, "stationary": 0}
-        self.co2_footprint = {"total_footprint": 0, "stationary": 0}
-        self.mki_footprint = {"total_footprint": 0, "stationary": 0}
-
-    @property
-    def power(self):
-        return 2 * (self.current_speed * self.resistance * 10 ** -3)  # kW
-
-    def calculate_energy_consumption(self):
-        """Calculation of energy consumption based on total time in system and properties"""
-
-        stationary_phase_indicator = [
-            "Doors closing stop",
-            "Converting chamber stop",
-            "Doors opening stop",
-            "aiting to pass lock stop",
-        ]
-
-        times = self.log["Timestamp"]
-        messages = self.log["Message"]
-
-        for i in range(len(times) - 1):
-            delta_t = times[i + 1] - times[i]
-
-            if messages[i + 1] in stationary_phase_indicator:
-                energy_delta = self.power * delta_t / 3600  # KJ/3600
-
-                self.energy_use["total_energy"] += energy_delta * 0.15
-                self.energy_use["stationary"] += energy_delta * 0.15
-
-            else:
-                self.energy_use["total_energy"] += self.power * delta_t / 3600
 
 
 class Routeable:
-    """Something with a route (networkx format)
+    """Mixin class: Something with a route (networkx format)
     route: a networkx path"""
 
     def __init__(self, route, complete_path=None, *args, **kwargs):
@@ -313,9 +286,7 @@ class Routeable:
 
 
 class IsLock(HasResource, Identifiable, Log):
-    """
-    Create a lock object
-
+    """Mixin class: Something has lock object properties
     properties in meters
     operation in seconds
     """
@@ -411,8 +382,7 @@ class IsLock(HasResource, Identifiable, Log):
 
 
 class Movable(Locatable, Routeable, Log):
-    """Movable class
-
+    """Mixin class: Something can move
     Used for object that can move with a fixed speed
     geometry: point used to track its current location
     v: speed"""
@@ -426,12 +396,12 @@ class Movable(Locatable, Routeable, Log):
     def move(self):
         """determine distance between origin and destination, and
         yield the time it takes to travel it
-        
+
         Assumption is that self.path is in the right order - vessel moves from route[0] to route[-1].
         """
         self.distance = 0
 
-        # Check if vessel is at correct location - if note, move to location
+        # Check if vessel is at correct location - if not, move to location
         if (
             self.geometry
             != nx.get_node_attributes(self.env.FG, "geometry")[self.route[0]]
@@ -456,8 +426,9 @@ class Movable(Locatable, Routeable, Log):
         for node in enumerate(self.route):
             self.node = node[1]
 
-            origin = self.route[node[0]]
-            destination = self.route[node[0] + 1]
+            if node[0] + 2 <= len(self.route):
+                origin = self.route[node[0]]
+                destination = self.route[node[0] + 1]
             edge = self.env.FG.edges[origin, destination]
 
             if "Lock" in edge.keys():
@@ -479,12 +450,14 @@ class Movable(Locatable, Routeable, Log):
                 yield from self.pass_lock(origin, destination, lock_id)
 
             else:
+                # print('I am going to go to the next node {}'.format(destination))
                 yield from self.pass_edge(origin, destination)
 
             if node[0] + 2 == len(self.route):
                 break
 
-        self.geometry = nx.get_node_attributes(self.env.FG, "geometry")[destination]
+        # self.geometry = nx.get_node_attributes(self.env.FG, "geometry")[destination]
+
         logger.debug("  distance: " + "%4.2f" % self.distance + " m")
         logger.debug("  sailing:  " + "%4.2f" % self.current_speed + " m/s")
         logger.debug(
@@ -498,67 +471,70 @@ class Movable(Locatable, Routeable, Log):
         orig = nx.get_node_attributes(self.env.FG, "geometry")[origin]
         dest = nx.get_node_attributes(self.env.FG, "geometry")[destination]
 
-        distance = self.wgs84.inv(
-            shapely.geometry.asShape(orig).x,
-            shapely.geometry.asShape(orig).y,
-            shapely.geometry.asShape(dest).x,
-            shapely.geometry.asShape(dest).y,
-        )[2]
+        if 'geometry' in edge:
+            edge_route = np.array(edge['geometry'])
 
-        self.distance += distance
-        arrival = self.env.now
+            # check if edge is in the sailing direction, otherwise flip it
+            distance_from_start = self.wgs84.inv(
+                    orig.x,
+                    orig.y,
+                    edge_route[0][0],
+                    edge_route[0][1],
+                )[2]
+            distance_from_stop = self.wgs84.inv(
+                    orig.x,
+                    orig.y,
+                    edge_route[-1][0],
+                    edge_route[-1][1],
+                )[2]
+            if distance_from_start>distance_from_stop:
+                # when the distance from the starting point is greater than from the end point
+                edge_route = np.flipud(np.array(edge['geometry']))
 
-        # Act based on resources
-        if "Resources" in edge.keys():
-            with self.env.FG.edges[origin, destination][
-                "Resources"
-            ].request() as request:
-                yield request
+            for index, pt in enumerate(edge_route[:-1]):
+                sub_orig = shapely.geometry.Point(edge_route[index][0], edge_route[index][1])
+                sub_dest = shapely.geometry.Point(edge_route[index+1][0], edge_route[index+1][1])
 
-                if arrival != self.env.now:
-                    self.log_entry(
-                        "Waiting to pass edge {} - {} start".format(
-                            origin, destination
-                        ),
-                        arrival,
-                        0,
-                        orig,
-                    )
-                    self.log_entry(
-                        "Waiting to pass edge {} - {} stop".format(origin, destination),
-                        self.env.now,
-                        0,
-                        orig,
-                    )
-
-                self.log_entry(
-                    "Sailing from node {} to node {} start".format(origin, destination),
-                    self.env.now,
-                    0,
-                    orig,
-                )
+                distance = self.wgs84.inv(
+                    shapely.geometry.asShape(sub_orig).x,
+                    shapely.geometry.asShape(sub_orig).y,
+                    shapely.geometry.asShape(sub_dest).x,
+                    shapely.geometry.asShape(sub_dest).y,
+                )[2]
+                self.distance += distance
+                self.log_entry("Sailing from node {} to node {} sub edge {} start".format(origin, destination, index), self.env.now, 0, sub_orig,)
                 yield self.env.timeout(distance / self.current_speed)
-                self.log_entry(
-                    "Sailing from node {} to node {} stop".format(origin, destination),
-                    self.env.now,
-                    0,
-                    dest,
-                )
-
+                self.log_entry("Sailing from node {} to node {} sub edge {} stop".format(origin, destination, index), self.env.now, 0, sub_dest,)
+            self.geometry = dest
+            # print('   My new origin is {}'.format(destination))
         else:
-            self.log_entry(
-                "Sailing from node {} to node {} start".format(origin, destination),
-                self.env.now,
-                0,
-                orig,
-            )
-            yield self.env.timeout(distance / self.current_speed)
-            self.log_entry(
-                "Sailing from node {} to node {} start".format(origin, destination),
-                self.env.now,
-                0,
-                dest,
-            )
+            distance = self.wgs84.inv(
+                shapely.geometry.asShape(orig).x,
+                shapely.geometry.asShape(orig).y,
+                shapely.geometry.asShape(dest).x,
+                shapely.geometry.asShape(dest).y,
+            )[2]
+
+            self.distance += distance
+            arrival = self.env.now
+
+            # Act based on resources
+            if "Resources" in edge.keys():
+                with self.env.FG.edges[origin, destination]["Resources"].request() as request:
+                    yield request
+
+                    if arrival != self.env.now:
+                        self.log_entry("Waiting to pass edge {} - {} start".format(origin, destination), arrival, 0, orig,)
+                        self.log_entry("Waiting to pass edge {} - {} stop".format(origin, destination), self.env.now, 0, orig,)
+
+                    self.log_entry("Sailing from node {} to node {} start".format(origin, destination), self.env.now, 0, orig,)
+                    yield self.env.timeout(distance / self.current_speed)
+                    self.log_entry("Sailing from node {} to node {} stop".format(origin, destination), self.env.now, 0, dest,)
+
+            else:
+                self.log_entry("Sailing from node {} to node {} start".format(origin, destination), self.env.now, 0, orig,)
+                yield self.env.timeout(distance / self.current_speed)
+                self.log_entry("Sailing from node {} to node {} stop".format(origin, destination), self.env.now, 0, dest,)
 
     def pass_lock(self, origin, destination, lock_id):
         """Pass the lock"""
@@ -576,18 +552,8 @@ class Movable(Locatable, Routeable, Log):
 
         if wait_for_waiting_area != self.env.now:
             waiting = self.env.now - wait_for_waiting_area
-            self.log_entry(
-                "Waiting to enter waiting area start",
-                wait_for_waiting_area,
-                0,
-                self.geometry,
-            )
-            self.log_entry(
-                "Waiting to enter waiting area stop",
-                self.env.now,
-                waiting,
-                self.geometry,
-            )
+            self.log_entry("Waiting to enter waiting area start", wait_for_waiting_area, 0, nx.get_node_attributes(self.env.FG, "geometry")[origin],)
+            self.log_entry("Waiting to enter waiting area stop", self.env.now, waiting, nx.get_node_attributes(self.env.FG, "geometry")[destination],)
 
         # Request access to line-up area
         wait_for_lineup_area = self.env.now
@@ -598,18 +564,12 @@ class Movable(Locatable, Routeable, Log):
 
         if wait_for_lineup_area != self.env.now:
             waiting = self.env.now - wait_for_lineup_area
-            self.log_entry(
-                "Waiting in waiting area start", wait_for_lineup_area, 0, self.geometry
-            )
-            self.log_entry(
-                "Waiting in waiting area stop", self.env.now, waiting, self.geometry
-            )
+            self.log_entry("Waiting in waiting area start", wait_for_lineup_area, 0, nx.get_node_attributes(self.env.FG, "geometry")[origin])
+            self.log_entry("Waiting in waiting area stop", self.env.now, waiting, nx.get_node_attributes(self.env.FG, "geometry")[destination])
 
         # Request access to lock
         wait_for_lock_entry = self.env.now
-        access_lock = lock.resource.request(
-            priority=-1 if origin == lock.water_level else 0
-        )
+        access_lock = lock.resource.request(priority=-1 if origin == lock.water_level else 0)
         yield access_lock
 
         # Shift water level
@@ -620,15 +580,11 @@ class Movable(Locatable, Routeable, Log):
 
         if wait_for_lock_entry != self.env.now:
             waiting = self.env.now - wait_for_lock_entry
-            self.log_entry(
-                "Waiting in line-up area start", wait_for_lock_entry, 0, self.geometry
-            )
-            self.log_entry(
-                "Waiting in line-up area stop", self.env.now, waiting, self.geometry
-            )
+            self.log_entry("Waiting in line-up area start", wait_for_lock_entry, 0, nx.get_node_attributes(self.env.FG, "geometry")[origin])
+            self.log_entry("Waiting in line-up area stop", self.env.now, waiting, nx.get_node_attributes(self.env.FG, "geometry")[destination])
 
         # Vessel inside the lock
-        self.log_entry("Passing lock start", self.env.now, 0, self.geometry)
+        self.log_entry("Passing lock start", self.env.now, 0, nx.get_node_attributes(self.env.FG, "geometry")[origin])
 
         # Shift the water level
         yield from lock.convert_chamber(self.env, destination)
@@ -636,12 +592,7 @@ class Movable(Locatable, Routeable, Log):
         # Vessel outside the lock
         lock.resource.release(access_lock)
         passage_time = lock.doors_close + lock.operating_time + lock.doors_open
-        self.log_entry(
-            "Passing lock stop",
-            self.env.now,
-            passage_time,
-            nx.get_node_attributes(self.env.FG, "geometry")[destination],
-        )
+        self.log_entry("Passing lock stop", self.env.now, passage_time, nx.get_node_attributes(self.env.FG, "geometry")[destination],)
 
     @property
     def current_speed(self):
@@ -650,7 +601,6 @@ class Movable(Locatable, Routeable, Log):
 
 class ContainerDependentMovable(Movable, HasContainer):
     """ContainerDependentMovable class
-
     Used for objects that move with a speed dependent on the container level
     compute_v: a function, given the fraction the container is filled (in [0,1]), returns the current speed"""
 
