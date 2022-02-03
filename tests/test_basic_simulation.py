f1c376def2950e631ba29b151889162089c49000
Fedor Baart <fedor.baart@deltares.nl>
2022-02-03 21:05:12 +0100

    extra unit test for moving ship

--- tests/test_basic_simulation.py
+++ tests/test_basic_simulation.py
@@ -27,25 +27,19 @@ import math
 # Used for making the graph to visualize our problem
 import networkx as nx
 
+import pytest
+
 # Creating the test objects
 
 
+@pytest.fixture
+def nodes():
+    Node = type("Site", (opentnsim.core.Identifiable, opentnsim.core.Locatable), {})
 
-# Actual testing starts here
-def test_basic_simulation():
-    # 1. create graph
-    FG = nx.DiGraph()
-
-    Node = type('Site', (opentnsim.core.Identifiable, opentnsim.core.Locatable), {})
-
-    data_node_1 = {"name": "Node 1",
-                   "geometry": shapely.geometry.Point(0, 0)}
-    data_node_2 = {"name": "Node 2",
-                   "geometry": shapely.geometry.Point(0.1, 0)}
-    data_node_3 = {"name": "Node 3",
-                   "geometry": shapely.geometry.Point(0.2, 0)}
-    data_node_4 = {"name": "Node 4",
-                   "geometry": shapely.geometry.Point(0.3, 0)}
+    data_node_1 = {"name": "Node 1", "geometry": shapely.geometry.Point(0, 0)}
+    data_node_2 = {"name": "Node 2", "geometry": shapely.geometry.Point(0.1, 0)}
+    data_node_3 = {"name": "Node 3", "geometry": shapely.geometry.Point(0.2, 0)}
+    data_node_4 = {"name": "Node 4", "geometry": shapely.geometry.Point(0.3, 0)}
 
     node_1 = Node(**data_node_1)
     node_2 = Node(**data_node_2)
@@ -53,40 +47,56 @@ def test_basic_simulation():
     node_4 = Node(**data_node_4)
 
     nodes = [node_1, node_2, node_3, node_4]
+    return nodes
+
+
+@pytest.fixture
+def vessel():
+    # 2. Create vessel
+    # Make your preferred class out of available mix-ins.
+    TransportResource = type(
+        "Vessel",
+        (opentnsim.core.Identifiable, opentnsim.core.Movable, opentnsim.core.Routeable),
+        {},
+    )
+
+    # Create a dict with all important settings
+    data_vessel = {
+        "env": None,
+        "name": None,
+        "route": None,
+        "geometry": None,
+        "v": 4,
+    }  # constant speed of the vessel
+
+    vessel = TransportResource(**data_vessel)
+    return vessel
+
+
+# Actual testing starts here
+def test_basic_simulation(nodes, vessel):
+    # 1. create graph
+    FG = nx.DiGraph()
 
     positions = {}
     for node in nodes:
         positions[node.name] = (node.geometry.x, node.geometry.y)
         FG.add_node(node.name, geometry=node.geometry)
 
-    path = [[node_1, node_2],  # From node 1 to node 2
-            [node_2, node_3],  # From node 2 to node 3
-            [node_3, node_4],  # From node 3 to node 4
-            [node_4, node_3],  # From node 4 to node 3
-            [node_3, node_2],  # From node 3 to node 2
-            [node_2, node_1]]  # From node 2 to node 1
+    path = [
+        [nodes[0], nodes[1]],  # From node 1 to node 2
+        [nodes[1], nodes[2]],  # From node 2 to node 3
+        [nodes[2], nodes[3]],  # From node 3 to node 4
+        [nodes[3], nodes[2]],  # From node 4 to node 3
+        [nodes[2], nodes[1]],  # From node 3 to node 2
+        [nodes[1], nodes[0]],
+    ]  # From node 2 to node 1
 
     for edge in path:
         FG.add_edge(edge[0].name, edge[1].name, weight=1)
 
-    # 2. Create vessel
-    # Make your preferred class out of available mix-ins.
-    TransportResource = type('Vessel',
-                             (opentnsim.core.Identifiable,
-                              opentnsim.core.Movable,
-                              opentnsim.core.Routeable), {})
-
-    # Create a dict with all important settings
-    data_vessel = {"env": None,
-                   "name": None,
-                   "route": None,
-                   "geometry": None,
-                   "v": 4}  # constant speed of the vessel
-
-    vessel = TransportResource(**data_vessel)
-
     # 3. Define path
-    path = nx.dijkstra_path(FG, node_1.name, node_4.name)
+    path = nx.dijkstra_path(FG, nodes[0].name, nodes[3].name)
 
     # 4. Run simulation
     # Start simpy environment
@@ -99,21 +109,24 @@ def test_basic_simulation():
 
     # Add environment and path to the vessel
     vessel.env = env  # the created environment
-    vessel.name = 'Vessel No.1'
+    vessel.name = "Vessel No.1"
     vessel.route = path  # the route (the sequence of nodes, as stored as the second column in the path)
     vessel.geometry = env.FG.nodes[path[0]][
-        'geometry']  # a shapely.geometry.Point(lon,lat) (here taken as the starting node of the vessel)
+        "geometry"
+    ]  # a shapely.geometry.Point(lon,lat) (here taken as the starting node of the vessel)
 
     # Start the simulation
     env.process(vessel.move())
     env.run()
 
+    # vessel should have reached end of path
+    assert vessel.geometry == env.FG.nodes[path[-1]]["geometry"]
+
     # 5. Test output
     def calculate_distance(orig, dest):
-        wgs84 = pyproj.Geod(ellps='WGS84')
+        wgs84 = pyproj.Geod(ellps="WGS84")
 
-        distance = wgs84.inv(orig[0], orig[1],
-                             dest[0], dest[1])[2]
+        distance = wgs84.inv(orig[0], orig[1], dest[0], dest[1])[2]
 
         return distance
 
@@ -121,11 +134,23 @@ def test_basic_simulation():
     vessel_path_t = []
 
     for t in range(0, len(vessel.log["Message"])):
-        vessel_path_x0 = (vessel.log["Geometry"][0].x, vessel.log['Geometry'][0].y)
-        vessel_path_t0 = vessel.log["Timestamp"][0].timestamp()/60
+        vessel_path_x0 = (vessel.log["Geometry"][0].x, vessel.log["Geometry"][0].y)
+        vessel_path_t0 = vessel.log["Timestamp"][0].timestamp() / 60
         vessel_path_x.append(
-            calculate_distance(vessel_path_x0, (vessel.log["Geometry"][t].x, vessel.log['Geometry'][t].y)))
-        vessel_path_t.append(vessel.log["Timestamp"][t].timestamp() / 60 - vessel_path_t0)
+            calculate_distance(
+                vessel_path_x0,
+                (vessel.log["Geometry"][t].x, vessel.log["Geometry"][t].y),
+            )
+        )
+        vessel_path_t.append(
+            vessel.log["Timestamp"][t].timestamp() / 60 - vessel_path_t0
+        )
 
     # Test if max distance / speed roughly is equal to simulation time (NB: should be exactly the same (bug))
-    np.testing.assert_almost_equal((vessel_path_x[-1] / vessel.v) / 60, vessel_path_t[-1], decimal=7, err_msg='', verbose=True)
+    np.testing.assert_almost_equal(
+        (vessel_path_x[-1] / vessel.v) / 60,
+        vessel_path_t[-1],
+        decimal=7,
+        err_msg="",
+        verbose=True,
+    )
