62f86231e89037618288aacfe55d1f3b18d84012
mvgijn <maxwvangijn@gmail.com>
2023-04-11 14:20:55 +0200

    Code used for nausbot simulation of experiment 3 for CIRCLE graph

--- notebooks/student_notebooks/max/tactics.py
+++ notebooks/student_notebooks/max/tactics.py
@@ -17,6 +17,47 @@ import matplotlib.pyplot as plt
 
 import opentnsim.core
 
+# Maximum CO2 emission for normalization
+# Maximum PM10 emission for normalization
+
+MAX_CO2 = 3119.526428
+MAX_PM10 = 2.056981 
+
+# We can't compute the true hotel power emission
+# So we'll estimate it
+# We'll use the 0.6 engine order emission / duration * 2.44
+WAIT_EMISSION_PM10_PER_SECOND = 0.817459 / 23.846344 * 2.44
+
+def compute_passages(vessel):
+    """compute the passages of vessel on some edges of interest"""
+    log_df = pd.DataFrame(vessel.log)
+    
+    c_d = 'Sailing from node C to node D sub edge 0 stop'
+    e_d = 'Sailing from node E to node D sub edge 0 stop'
+    c_d_idx = log_df['Message'] == c_d
+    e_d_idx = log_df['Message'] == e_d
+    c_d_df = log_df[c_d_idx]
+    e_d_df = log_df[e_d_idx]
+    
+    # time of arrival of first passage from c to d
+    if c_d_df.shape[0] >= 1:
+        c_d_t = c_d_df.iloc[0]['Timestamp'] - vessel.env.simulation_start
+        c_d_t = c_d_t.total_seconds()
+    else:
+        c_d_t = None
+
+    # time of arrival of first passage from e to d
+    if e_d_df.shape[0] >= 1:
+        e_d_t = e_d_df.iloc[0]['Timestamp'] - vessel.env.simulation_start
+        e_d_t = e_d_t.total_seconds()
+    else:
+        e_d_t = None
+    return {
+        "c_d_t": c_d_t,
+        "e_d_t": e_d_t
+    }
+        
+
 def run_simulation(geometry, route, graph, engine_order=0.8):
     Vessel = type(
         'Vessel', 
@@ -57,6 +98,7 @@ def run_simulation(geometry, route, graph, engine_order=0.8):
     simulation_start = datetime.datetime.now()
     env = simpy.Environment(initial_time = time.mktime(simulation_start.timetuple()))
     env.FG = graph
+    env.simulation_start = simulation_start
 
     # add environment to the vessel, and specify the vessels route and current location (beginning of the path)
     vessel.env = env
@@ -71,7 +113,7 @@ def run_simulation(geometry, route, graph, engine_order=0.8):
     #Determine trip time
     start_time = simulation_start.timestamp()
         
-
+    passages = compute_passages(vessel)
     end = vessel.log["Timestamp"][-1]
 
     end_time = end.timestamp()
@@ -82,7 +124,14 @@ def run_simulation(geometry, route, graph, engine_order=0.8):
     # create dataframe from energy calculation computation
     energy_df = pd.DataFrame.from_dict(energycalculation.energy_use)
 
-    return end_time-start_time, end, energy_df, vessel
+    results =  {
+        "duration": end_time-start_time, 
+        "eta": end, 
+        "energy_df": energy_df, 
+        "vessel": vessel
+    }
+    results.update(passages)
+    return results
 
 
 
@@ -93,11 +142,11 @@ def generate_route_alternatives(graph):
     route_alternatives = [
         {
             "name": "direct",
-            "waypoints": ['A', 'F', 'H', 'L']
+            "waypoints": ['A', 'E', 'H', 'L']
         },
         {
             "name": "redirect",
-            "waypoints": ['A', 'F', 'L', 'F']
+            "waypoints": ['A', 'F','I', 'L', 'J', 'F']
         },
     ]
 
@@ -123,7 +172,7 @@ def generate_route_alternatives(graph):
 
 def generate_engine_alternatives():
     """generate a list of dictionaries, where each dictionary is an engine order setting."""
-    engine_orders = [0.5, 0.6, 0.7, 0.8, 0.9]
+    engine_orders = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     engine_alternatives = []
     for engine_order in engine_orders:
         alternative = {"engine_order": engine_order} 
@@ -167,9 +216,16 @@ def add_kpi(alternatives_df, berth_available, graph, geometry, visited_nodes, wa
         #current_node = row['route'][0]
         #geometry = graph.nodes[current_node]['geometry']
         engine_order = row['engine_order']
-        duration, eta, energy_df, vessel = run_simulation(geometry=geometry, route=remaining_route, graph=graph, engine_order=engine_order)
+        results = run_simulation(geometry=geometry, route=remaining_route, graph=graph, engine_order=engine_order)
+        duration = results["duration"]
+        eta = results["eta"]
+        energy_df = results["energy_df"]
+        vessel = results["vessel"]
+        c_d_t = results["c_d_t"]
+        e_d_t = results["e_d_t"]
 
         energy_sum = energy_df.sum(axis=0, numeric_only=True) 
+        
         result.at[idx, 'duration'] = duration
         result.at[idx, 'total_energy'] = energy_sum['total_energy']
         result.at[idx, 'total_emission_CO2'] = energy_sum['total_emission_CO2']
@@ -178,11 +234,30 @@ def add_kpi(alternatives_df, berth_available, graph, geometry, visited_nodes, wa
         result.at[idx, 'total_diesel_consumption_ICE_vol'] = energy_sum['total_diesel_consumption_ICE_vol']
         result.at[idx, 'vessel'] = vessel
         result.at[idx, 'remaining_route'] = remaining_route
+        result.at[idx, "c_d_t"] = c_d_t
+        result.at[idx, "e_d_t"] = e_d_t
     # TODO: replace with a timeout on_pass_edge 
     if not berth_available:    
-        # add waiting time to direct routes
-        result.loc[result['name'] == 'direct', 'duration']  += 10
-
+        # add waiting time to direct routes dependent on engine order
+        direct_idx = result["name"] == "direct"
+        redirect_idx = result["name"] == "redirect"
+        wait_time = 20 - result.loc[direct_idx, 'c_d_t']
+        result.loc[direct_idx, "wait_time"] = wait_time
+        result.loc[redirect_idx, "wait_time"] = 0
+    else:
+        result['wait_time'] = 0
+    result['wait_emission_pm10'] = result['wait_time'] * WAIT_EMISSION_PM10_PER_SECOND
+    # compute the sorting variables
+    result["strategy_duration"] = result["duration"] + result["wait_time"]
+    result["strategy_pm10"] = result['total_emission_PM10'] + result['wait_emission_pm10']
+    result["strategy_fuel"] = result["total_diesel_consumption_ICE_vol"]
+    # evenly weigh co2 and pm10 emissions (normalize and then mean)
+    result["strategy_mca"] = ((result["total_emission_CO2"] / MAX_CO2) + (result["strategy_pm10"] / MAX_PM10)) / 2
+    
+        
+        
+    
     return result
+    
 
     
