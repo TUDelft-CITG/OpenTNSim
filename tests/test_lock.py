e78a3e56421b7e6d92ff30d9028de4e4f06c60f9
CleanZwieber <f.p.bakker@tudelft.nl>
2024-02-08 14:28:22 +0100

    Test for lock module

---
+++ tests/test_lock.py
@@ -0,0 +1,85 @@
+import datetime
+import pytest
+import shapely
+import simpy
+import networkx as nx
+
+@pytest.fixture
+def graph():
+    FG = nx.DiGraph()
+    Node = type('Site', (core.Identifiable, core.Log, core.Locatable, core.HasResource), {})
+    coordinates = [(0, 0), (1200, 0)]
+    for index, coord in enumerate(coordinates):
+        node = Node(**{'env': [], 'name': index, 'geometry': shapely.geometry.Point(x, y)})
+        FG.add_node(node.name, geometry=node.geometry, Info={'geometry': geometry})
+
+    edges = [(FG.nodes[0], FG.nodes[1]), (FG.nodes[1], FG.nodes[0])]
+    for (node_start, node_stop) in edges:
+        geometry = geometry.LineString([node_start.geometry, node_stop.geometry])
+        FG.add_edge(node_start.name, node_stop.name, weight=1, geometry=geometry,
+                    Info={'geometry': geometry, 'length': geometry.length})
+
+    # run for a day
+    return FG
+
+
+@pytest.fixture
+def env(FG):
+    t_start = datetime.datetime(2024,1,1,0,0,0)
+    env = simpy.Environment(initial_time=t_start.timestamp())
+    env.epoch = t_start
+    env.FG = FG
+    # run for a day
+    return env
+
+
+@pytest.fixture
+def add_hydrodynamics(env):
+    t_end = datetime.datetime(2024, 2, 1, 0, 0, 0)
+    stations = list(env.FG.nodes)
+    times = np.arange(env.epoch,t_end,datetime.timedelta(seconds=600))
+    water_depth = np.linspace(10, 10, len(times))
+    water_level = [np.linspace(0, 0, len(times)), np.linspace(1, 1, len(times))]
+    salinity = [np.linspace(0, 0, len(times)), np.linspace(0, 0, len(times))]
+    station_data = xr.DataArray(data=stations,dims=["STATIONS"])
+    time_data = xr.DataArray(data=times,dims=["TIME"])
+    depth_data = xr.DataArray(data=water_depth,dims=["STATIONS"])
+    water_level_data = xr.DataArray(data=[wlev for wlev in water_level],dims=["STATIONS", "TIME"])
+    salinity_data = xr.DataArray(data=[sal for sal in salinity],dims=["STATIONS", "TIME"])
+    hydrodynamic_data = xr.Dataset({'TIME': time_data, 'Stations': station_data, 'MBL': depth_data,
+                                    'Water level': water_level_data, 'Salinity': salinity_data})
+    env.vessel_traffic_service = opentnsim.vessel_traffic_service.VesselTrafficService(hydrodynamic_data)
+    return
+
+
+def add_vessel(env,name,origin,destination,type,L,B,T,v,arrival_time):
+    Vessel = type('Vessel',
+                  (core.SimpyObject, core.Identifiable, lock.HasWaitingArea, lock.HasLock, lock.HasLineUpArea,
+                   core.Movable, vessel.VesselProperties, output.HasOutput, vessel.ExtraMetadata), {})
+
+    vessel = Vessel(**{'env':env, 'name':name, 'origin':origin, 'destination':destination,
+                       'geometry':env.FG.nodes[origin],'route':nx.dijkstra_path(env.FG,origin,destination),
+                       'type':type, 'L':L, 'B':B, 'T':T, 'v':v, 'arrival_time':arrival_time})
+
+    env.process(vessel.move())
+    return
+
+
+def lock_test(env):
+    lock = opentnsim.lock.IsLock(env=env, name='Lock', lock_length=400, lock_width=40, lock_depth=10,
+                                 node_doors1=0, node_doors2=1, distance_doors1_from_first_waiting_area=400,
+                                 distance_doors2_from_second_waiting_area=400, speed_reduction_factor = 1)
+    lock.IsLockLineUpArea(env=env, name='Lock', distance_to_lock_doors=0, start_node=0, end_node=1,
+                                          lineup_length=400, speed_reduction_factor = 1)
+    lock.IsLockLineUpArea(env=env, name="Lock", distance_to_lock_doors=0, start_node=1, end_node=0,
+                                          lineup_length=400, speed_reduction_factor = 1)
+    lock.IsLockWaitingArea(env=sim.environment, name='Lock', distance_from_node=0, node=0)
+    lock.IsLockWaitingArea(env=sim.environment, name="Lock", distance_from_node=0, node=1)
+
+    add_vessel(env, 0, 0, 1 ,None, 200, 30, 8, v, datetime.datetime(2024, 1, 1, 0, 0, 0))
+
+    env.run()
+
+    vessels = env.vessels
+
+    assert len(vessels[0].log) > 2
