4699f86889f4bddea755d1aface254f0b93dbd25
frank-klein-schaarsberg-wb <58854028+frank-klein-schaarsberg-wb@users.noreply.github.com>
2025-05-06 14:21:17 +0200

    restructuring core

---
+++ opentnsim/core/__init__.py
@@ -0,0 +1,6 @@
+from .resource import HasResource
+from .misc import Neighbours, ExtraMetadata
+from .vessel_properties import HasLength, HasLoad, VesselProperties
+from .container import HasContainer
+from .routable import Routable, Routeable
+from .movable import Movable, ContainerDependentMovable
