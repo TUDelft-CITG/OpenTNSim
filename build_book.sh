c2e2ef43939f8377d6fdf34955a696439ff5176b
Fleur van Alphen <fleur.van.alphen@witteveenbos.com>
2025-10-23 16:16:03 +0200

    kan dit nu weg?

--- build_book.sh
+++ build_book.sh
@@ -4,9 +4,6 @@ cp notebooks/*.ipynb book/examples/
 # copy the data from the notebooks folder
 mkdir -p book/examples/data/
 cp notebooks/data/* book/examples/data/
-# copy the figures from the notebooks folder
-mkdir -p book/examples/figures/
-cp notebooks/figures/* book/examples/figures/
 # Also copy figures to book root for accessibility from any location
 mkdir -p book/figures/
 cp notebooks/figures/* book/figures/
