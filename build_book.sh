d6d78335a47b823f9dc70035d15d8285ed0dcd34
Fleur van Alphen <fleur.van.alphen@witteveenbos.com>
2025-10-23 16:05:38 +0200

    fix afbeeldingen in notebooks

--- build_book.sh
+++ build_book.sh
@@ -6,7 +6,10 @@ mkdir -p book/examples/data/
 cp notebooks/data/* book/examples/data/
 # copy the figures from the notebooks folder
 mkdir -p book/examples/figures/
-cp notebooks/figures/*.png book/examples/figures/
+cp notebooks/figures/* book/examples/figures/
+# Also copy figures to book root for accessibility from any location
+mkdir -p book/figures/
+cp notebooks/figures/* book/figures/
 # Copy the api docs from the docs folder
 cp docs/opentnsim.rst book/docs
 # Copy the Authors list from the root folder
