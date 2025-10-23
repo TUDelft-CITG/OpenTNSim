5df3ae5795ef0be8a0b9aac7dbb29dda7b550b20
Fleur van Alphen <fleur.van.alphen@witteveenbos.com>
2025-10-23 16:30:52 +0200

    de figuren van voorbeelden moeten toch in een andere map...

--- build_book.sh
+++ build_book.sh
@@ -4,9 +4,9 @@ cp notebooks/*.ipynb book/examples/
 # copy the data from the notebooks folder
 mkdir -p book/examples/data/
 cp notebooks/data/* book/examples/data/
-# Also copy figures to book root for accessibility from any location
-mkdir -p book/figures/
-cp notebooks/figures/* book/figures/
+# copy the figures from the notebooks folder
+mkdir -p book/examples/figures/
+cp notebooks/figures/* book/examples/figures/
 # Copy the api docs from the docs folder
 cp docs/opentnsim.rst book/docs
 # Copy the Authors list from the root folder
