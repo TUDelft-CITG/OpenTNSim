2edadc2ac2864bbd56a1247000dff498c28b8914
Fedor Baart <fedor.baart@rws.nl>
2025-11-29 16:16:50 +0100

    Test euris network

--- tests/test_fis.py
+++ tests/test_fis.py
@@ -3,11 +3,15 @@
 import opentnsim.fis
 
 
-def test_network_v2():
+def test_fis_network_v2():
     """check if we can load the network"""
-    _ = opentnsim.fis.load_network(version="0.2")
+    _ = opentnsim.fis.load_network(etwork="fis", version="0.2")
 
 
-def test_network_v3():
+def test_fis_network_v3():
     """check if we can load the network"""
-    _ = opentnsim.fis.load_network(version="0.3")
+    _ = opentnsim.fis.load_network(etwork="fis", version="0.3")
+
+def test_euris_network_v1():
+    """check if we can load the network"""
+    _ = opentnsim.fis.load_network(etwork="euris", version="0.1")
