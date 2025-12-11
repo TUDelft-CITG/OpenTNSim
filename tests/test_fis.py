8c229da37664979465a4acee0aa0ddea65d2a406
SiggyF <f.baart@gmail.com>
2025-12-11 16:28:17 +0100

    fix typo

--- tests/test_fis.py
+++ tests/test_fis.py
@@ -5,13 +5,13 @@ import opentnsim.fis
 
 def test_fis_network_v2():
     """check if we can load the network"""
-    _ = opentnsim.fis.load_network(etwork="fis", version="0.2")
+    _ = opentnsim.fis.load_network(network="fis", version="0.2")
 
 
 def test_fis_network_v3():
     """check if we can load the network"""
-    _ = opentnsim.fis.load_network(etwork="fis", version="0.3")
+    _ = opentnsim.fis.load_network(network="fis", version="0.3")
 
 def test_euris_network_v1():
     """check if we can load the network"""
-    _ = opentnsim.fis.load_network(etwork="euris", version="0.1")
+    _ = opentnsim.fis.load_network(network="euris", version="0.1")
