4dae1dbfe307613c0b469194d0a4f693a0c30bff
CleanZwieber <f.p.bakker@tudelft.nl>
2026-03-12 16:10:20 +0100

    Example for a lock with energy consumption
    Work in progress, also got rid of the VTS (now hosted in the port module as the port authority)

--- opentnsim/vessel_traffic_service/__init__.py
+++
@@ -1,4 +0,0 @@
-from .vessel_traffic_service import VesselTrafficService
-from opentnsim.environment.mixins.hydrodynamics import HydrodynamicDataManager
-
-__all__ = ["VesselTrafficService", "HydrodynamicDataManager"]
